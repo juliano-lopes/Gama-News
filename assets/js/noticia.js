a493ee2f6bc59280ee00e865e52c2640043a823b
Suzana Aparecida Alves <Suzana.Alves@oboticario.com.br>
2021-05-06 10:51:48 -0300

    atualização branch

---
+++ assets/js/noticia.js
@@ -0,0 +1,60 @@
+function mostrarNoticia1(){
+	var noticia = document.getElementById("mostrarNoticia1").style.display;
+if(noticia == 'none'){
+	document.getElementById("mostrarNoticia1").style.display = 'block';
+	document.getElementById("mostrar1").style.display = 'none';
+}else{
+	document.getElementById("mostrarNoticia1").style.display = 'none';
+document.getElementById("mostrar1").style.display = 'block';
+}
+}
+function mostrarNoticia2(){
+	var noticia = document.getElementById("mostrarNoticia2").style.display;
+if(noticia == 'none'){
+	document.getElementById("mostrarNoticia2").style.display = 'block';
+	document.getElementById("mostrar2").style.display = 'none';
+}else{
+	document.getElementById("mostrarNoticia2").style.display = 'none';
+	document.getElementById("mostrar2").style.display = 'block';
+}
+}
+function mostrarNoticia3(){
+	var noticia = document.getElementById("mostrarNoticia3").style.display;
+if(noticia == 'none'){
+	document.getElementById("mostrarNoticia3").style.display = 'block';
+	document.getElementById("mostrar3").style.display = 'none';
+}else{
+	document.getElementById("mostrarNoticia3").style.display = 'none';
+	document.getElementById("mostrar3").style.display = 'block';
+}
+}
+function mostrarNoticia4(){
+	var noticia = document.getElementById("mostrarNoticia4").style.display;
+if(noticia == 'none'){
+	document.getElementById("mostrarNoticia4").style.display = 'block';
+	document.getElementById("mostrar4").style.display = 'none';
+}else{
+	document.getElementById("mostrarNoticia4").style.display = 'none';
+	document.getElementById("mostrar4").style.display = 'block';
+}
+}
+function mostrarNoticia5(){
+	var noticia = document.getElementById("mostrarNoticia5").style.display;
+if(noticia == 'none'){
+	document.getElementById("mostrarNoticia5").style.display = 'block';
+	document.getElementById("mostrar5").style.display = 'none';
+}else{
+	document.getElementById("mostrarNoticia5").style.display = 'none';
+	document.getElementById("mostrar5").style.display = 'block';
+}
+}
+function mostrarNoticia6(){
+	var noticia = document.getElementById("mostrarNoticia6").style.display;
+if(noticia == 'none'){
+	document.getElementById("mostrarNoticia6").style.display = 'block';
+	document.getElementById("mostrar6").style.display = 'none';
+}else{
+	document.getElementById("mostrarNoticia6").style.display = 'none';
+	document.getElementById("mostrar6").style.display = 'block';
+}
+}
