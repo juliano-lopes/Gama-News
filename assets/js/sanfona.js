05b5145a1ce8aa695490e119d3a26b8d5b09a477
Juliano Lopes <julopys@hotmail.com>
2021-05-08 19:13:10 -0300

    Adiciona arquivo sanfona.js no index.html e sobre.html e faz função sanfona funcionar  tanto em um quanto no outro.

--- assets/js/sanfona.js
+++ assets/js/sanfona.js
@@ -4,7 +4,7 @@ function mostrarOcultarElemento(botaoClicado) {
 	// verifica se o botão existe, se é verdadeiro, se não é nulo
 	if (botaoExpandido) {
 		// pega o pai do pai do botão e busca dentro dele com um seletor o elemento que tenha a classe corpo-perfil e o oculta:
-		botaoExpandido.parentNode.parentNode.querySelector(".corpo-perfil").style.display = "none";
+		botaoExpandido.parentNode.parentNode.querySelector('[class*="corpo-"]').style.display = "none";
 		// coloca o atributo aria-expanded com o valor falso, ou seja, recolhido:
 		botaoExpandido.setAttribute("aria-expanded", "false");
 	}
@@ -13,6 +13,6 @@ function mostrarOcultarElemento(botaoClicado) {
 		// se for diferente, então coloca o clicado como expandido:
 		botaoClicado.setAttribute("aria-expanded", "true");
 		// pega o pai do pai do botão e busca dentro dele com um seletor o elemento que tenha a classe corpo-perfil e o deixa visível:
-		botaoClicado.parentNode.parentNode.querySelector(".corpo-perfil").style.display = "block";
+		botaoClicado.parentNode.parentNode.querySelector('[class*="corpo-"]').style.display = "block";
 	}
 }
