8f46dbc2e251127857d08545625f05716fe1aa80
Marcelo Oliveira <marcelovl1970@hotmail.com>
2021-05-08 05:08:18 -0300

    Adicionado recurso aumenta e diminue fonte e arquivo, index.html atualisado.

--- assets/js/fonte.js
+++ assets/js/fonte.js
@@ -1,22 +1,24 @@
+        let incrementoDecremento = 2;
+        document.getElementById("aumentar-fonte").addEventListener("click", () => {
+            document.querySelectorAll("h1, h2, h3, h4, h5, h6, p, a, span, button").forEach(function (el) {
+                
+                let fonteOriginal = parseInt(window.getComputedStyle(el).getPropertyValue("font-size"));
+                let novaFonte = fonteOriginal+incrementoDecremento;
+                el.setAttribute("style", "font-size: " + novaFonte + "px");
+                console.log("a fonte foi " + fonteOriginal + ", e aumentada ficou " + novaFonte);
+            });
 
-            window.onload = function() {
-                var elementBody = document.querySelector('body');
-                var elementBtnIncreaseFont = document.getElementById('increase-font');
-                var elementBtnDecreaseFont = document.getElementById('decrease-font');
-                // Padrão de tamanho, equivale a 100% do valor definido no Body
-                var fontSize = 100;
-                // Valor de incremento ou decremento, equivale a 10% do valor do Body
-                 var increaseDecrease = 15;
 
-                // Evento de click para aumentar a fonte
-                elementBtnIncreaseFont.addEventListener('click', function(event) {
-                    fontSize = fontSize + increaseDecrease;
-                    elementBody.style.fontSize = fontSize + '%';
-                });
+        });
 
-                // Evento de click para diminuir a fonte
-                elementBtnDecreaseFont.addEventListener('click', function(event) {
-                    fontSize = fontSize - increaseDecrease;
-                    elementBody.style.fontSize = fontSize + '%';
-                });
-            }
+        document.getElementById("diminuir-fonte").addEventListener("click", () => {
+            document.querySelectorAll("h1, h2, h3, h4, h5, h6, p, a, span, button").forEach((el) => {
+                
+                let fonteOriginal = parseInt(window.getComputedStyle(el).getPropertyValue("font-size"));
+                let novaFonte = fonteOriginal-incrementoDecremento;
+                el.setAttribute("style", "font-size: " + novaFonte + "px");
+                console.log("a fonte foi " + fonteOriginal + ", e diminuida ficou " + novaFonte);
+            });
+
+
+        });
